7380f19e94d26ddc5356041bd6e5b0f1da620154
yvanddniyo <nyvan80076@gmail.com>
2026-06-05 16:12:22 +0200

    Working pretty well.

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -3,18 +3,20 @@ from typing import List
 
 class Solution:
     def two_sum(self, nums: List[int], target: int):
-        prevHash = {}
-        for i, num in enumerate(nums):
+        obj = {}
+        for index, num in enumerate(nums):
+            print("print", obj)
             diff = target - num
-            if diff in prevHash:
-                return [prevHash[diff], i]
-            prevHash[num] = i
+            if diff in obj:
+                return [obj[diff], index]
+            else:
+                obj[num] = index
         return
 
 
 nums = [2, 7, 11, 15]
-target = 9
+target = 18
 
 s = Solution()
 result = s.two_sum(nums, target)
-print(result)
+print("->", result)
